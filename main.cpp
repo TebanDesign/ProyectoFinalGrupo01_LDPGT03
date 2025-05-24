a76b57c997642c4a8d26a6550149dcadba4215b2
soyeileen <rr24044@ues.edu.sv>
2025-05-24 15:13:42 -0600

    Agregadas modificaciones

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 // Descripción: Punto de entrada del sistema, contiene el menú principal.
 #include "menu.h"
+#include <windows.h> 
 
 int main() {
+   SetConsoleCP(CP_UTF8);
+   SetConsoleOutputCP(CP_UTF8);
+
     mostrarMenu();
     return 0;
 }
