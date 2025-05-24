b03b88c89a465f1e7508a8e38f2b8564148c6e65
Benitez1403 <167653468+Benitez1403@users.noreply.github.com>
2025-05-23 23:07:39 -0600

    Add console configuration function and initial medicamentos data file

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 // Descripción: Punto de entrada del sistema, contiene el menú principal.
 #include "menu.h"
 
+#include <locale> 
+#ifdef _WIN32
+#include <windows.h>  // Libreria necesaria para tildes
+#endif
+
+using namespace std;
+
+void configurarConsola() {
+    #ifdef _WIN32
+        SetConsoleOutputCP(65001);  // setea UTF-8 en Windows
+        SetConsoleCP(65001);
+    #else
+        setlocale(LC_ALL, "es_ES.UTF-8");  // Linux/Mac, Espero que funcione porque no tengo linux para probarlo xd
+    #endif
+}
+
 int main() {
+    configurarConsola(); 
     mostrarMenu();
     return 0;
 }
